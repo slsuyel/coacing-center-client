90be3391b04ead5351d557a0742a34f848438723
Suyel Haque <slsuyel@gmail.com>
2023-08-08 16:31:37 +0600

    Payment Failed page

--- src/components/PaymentFailed.jsx
+++ src/components/PaymentFailed.jsx
@@ -1,10 +1,29 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Container, Row, Col } from 'reactstrap';
 
 const PaymentFailed = () => {
     return (
-        <div>
-            <img src="https://lf16-adcdn-va.ibytedtos.com/obj/i18nblog/tt4b_cms/en-US/5jqet0ab9qci-1ecN9vLP4oiWG78EJOs4UJ.png" alt="" className='img-fluid w-100' />
-        </div>
+        <Container className="mt-5 pt-5">
+            <Row>
+                <Col md={{ size: 6, offset: 3 }} className='card'>
+                    <div className="text-center">
+                        <h1 className='my-3'>OPPS!! <br /> Payment Failed</h1>
+                        <p>Your payment has been Failed </p>
+                        <p>Please try again !</p>
+                    </div>
+                    <div className='text-center my-4'>
+                        <Link style={{ backgroundColor: 'rgb(22, 2, 149)' }}
+                            className="btn fw-bold text-warning"
+                            to="/"
+                        >
+                            Go to home
+                        </Link>
+                    </div>
+                </Col>
+
+            </Row>
+        </Container>
     );
 };
 
--- src/components/PaymentSuccess.jsx
+++ src/components/PaymentSuccess.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Container, Row, Col } from 'reactstrap';
 
 const PaymentSuccess = () => {
     return (
-        <div>
-            <img className='img-fluid w-100' src="https://shots.codepen.io/username/pen/YmVKzm-800.jpg?version=1564501659" alt="" />
-        </div>
+        <Container className="mt-5 pt-5">
+            <Row>
+                <Col md={{ size: 6, offset: 3 }} className='card'>
+                    <div className="text-center">
+                        <h1 className='my-3'>Payment Successful</h1>
+                        <p>Your payment has been successfully processed.</p>
+                        <p>Thank you for your purchase!</p>
+                    </div>
+                    <div className='text-center my-4'>
+                        <Link style={{ backgroundColor: 'rgb(22, 2, 149)' }}
+                            className="btn fw-bold text-warning"
+                            to="/"
+                        >
+                            Go to home
+                        </Link>
+                    </div>
+                </Col>
+            </Row>
+        </Container>
     );
 };
 
